61eb7ebcd38e4bafa824c442ab9495c662696e5d
Crowdbotics <team@crowdbotics.com>
2024-02-16 13:57:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "whole-person-data-h-46391",
+  url: "https://whole-person-data-h-46391.botics.co",
+  api: "https://whole-person-data-h-46391.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
